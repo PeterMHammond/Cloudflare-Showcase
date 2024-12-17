ecaeea5c1030a8097da69a3de1076069de9d38b2
Peter Hammond <peter@everygoodwork.online>
2024-12-16 19:54:09 -0800

    docs: add project description, repository, and license; - refactor fetch handler to include routing and 404 response in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,25 @@
 use worker::*;
+pub mod routes {
+    pub mod about;
+}
 
 #[event(fetch)]
-async fn fetch(
-    _req: Request,
-    _env: Env,
-    _ctx: Context,
-) -> Result<Response> {
+async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     console_error_panic_hook::set_once();
-    Response::ok("Jesus is Lord!")
+    
+    let router = Router::new();
+
+    let response = router
+        .get("/", |_, _| Response::ok("Jesus is Lord!"))
+        .get_async("/about", routes::about::handler)
+        .run(req, env)
+        .await?;
+
+    if response.status_code() == 404 {
+        return Response::from_html(
+            "<h1>404 - God exists, but this page doesn't.</h1><p>For since the creation of the world His invisible attributes, both His eternal power and divine nature, have been clearly seen, being understood through what has been made, so that they are without excuse. - Romans 1:20 LSB</p>"
+        ).map(|resp| resp.with_status(404));
+    }
+
+    Ok(response)
 }
---
+++ src/routes/about.rs
@@ -0,0 +1,28 @@
+use worker::*;
+
+// For development usage to show information about the application TODO: remove this before production deployment
+pub async fn handler(req: Request, _ctx: RouteContext<()>) -> Result<Response> {
+    let url = req.url()?;
+    let root_url = format!("{}://{}/", url.scheme(), url.host_str().unwrap_or("localhost"));
+
+    let version = option_env!("CARGO_PKG_VERSION").unwrap_or_default();
+    let name = option_env!("CARGO_PKG_NAME").unwrap_or_default();
+    let authors = option_env!("CARGO_PKG_AUTHORS").unwrap_or_default();
+    let description = option_env!("CARGO_PKG_DESCRIPTION").unwrap_or_default();
+    let repository = option_env!("CARGO_PKG_REPOSITORY").unwrap_or_default();
+    let license = option_env!("CARGO_PKG_LICENSE").unwrap_or_default();
+
+    let response_body = format!(
+        "Package: {name}\n\
+        Version: {version}\n\
+        Authors: {authors}\n\
+        Description: {description}\n\
+        Repository: {repository}\n\
+        License: {license}\n\
+        Current Route: {url}\n\n\
+        Available Routes:\n\
+        - GET /about\n    Shows this diagnostic information\n    Example: curl -X GET {root_url}about\n"
+    );
+
+    Response::ok(response_body)
+} 
